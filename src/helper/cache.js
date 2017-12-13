c160bd429aa48d5f9de9301367eb6b4c76f4ec69
wujian <wujian@acfun.tv>
2017-12-13 17:29:29 +0800

    add gzip

---
+++ src/helper/cache.js
@@ -0,0 +1,34 @@
+const {cache} = require('../config/defaultConfig');
+
+function refreshRes(stats, res) {
+    const {maxAge, expires, cacheControl, lastModified, etag} = cache;
+    if (expires){
+        res.setHeader('expires', (new Date(Date.now() + maxAge * 1000)).toUTCString());
+    }
+    if (cacheControl){
+        res.setHeader('Cache-Control', `public, max-age=${maxAge}`);
+    }
+    if (lastModified) {
+        res.setHeader('Last-Modified', stats.mtime.toUTCString());
+    }
+    if (etag) {
+        res.setHeader('ETag', `${stats.size}-${stats.mtime}`);
+    }
+}
+
+module.exports = function isFresh(stats, req, res) {
+    refreshRes(stats, res);
+    const lastModified = req.headers['if-modified-since'];
+    const etag = req.headers['if-none-match'];
+    if(!lastModified && !etag){
+        return false;
+    }
+    if (lastModified && lastModified !== res.getHeader('lastModified')){
+        return false;
+    }
+    if (etag && etag !== res.getHeader('ETag')){
+        return false;
+    }
+
+    return true;
+}
--- src/helper/route.js
+++ src/helper/route.js
@@ -6,6 +6,7 @@ const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 const config = require('../config/defaultConfig');
 const mime = require('./mime');
+const compress = require('./compress');
 
 const tplPath = path.join(__dirname, '../template/dir.pug');
 const source = fs.readFileSync(tplPath,'utf-8');
@@ -20,7 +21,11 @@ module.exports = async function (req, res, filePath) {
             // fs.readFile(filePath, (err, data) => { //这种异步的写法很慢，需要将全部的文件读取下来才会进行下一步
             //     res.end(data);
             // })
-            fs.createReadStream(filePath).pipe(res);
+            let rs = fs.createReadStream(filePath);
+            if (filePath.match(config.compress)) {
+                rs = compress(rs, req, res); 
+            };
+            rs.pipe(res);
         } else if (stats.isDirectory()){
             const files = await readdir(filePath);
             res.statusCode = 200;
