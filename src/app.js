3239205a1b2f0f8c446ef4053ffac3d23f1648d9
wujian <wujian@acfun.tv>
2017-12-14 14:46:30 +0800

    cli add

--- src/app.js
+++ src/app.js
@@ -4,12 +4,24 @@ const path = require('path');
 const chalk = require('chalk');
 const route = require('./helper/route');
 
-const server = http.createServer((req, res)=>{
-    const filePath = path.join(conf.root, req.url);
-    route(req, res, filePath);
-});
+class Server{
 
-server.listen(conf.port, conf.hostname, () =>{
-    const addr = `http://${conf.hostname}:${conf.port}`;
-    console.info(`Server started at ${chalk.green(addr)}`);
-});
+    constructor (config){
+        this.conf = Object.assign({}, conf, config);
+    }
+
+    start() {
+        const server = http.createServer((req, res)=>{
+            const filePath = path.join(this.conf.root, req.url);
+            route(req, res, filePath, this.conf);
+        });
+        
+        server.listen(this.conf.port, this.conf.hostname, () =>{
+            const addr = `http://${this.conf.hostname}:${this.conf.port}`;
+            console.info(`Server started at ${chalk.gray(addr)}`);
+        });
+    }
+
+}
+
+module.exports = Server;
--- src/helper/route.js
+++ src/helper/route.js
@@ -4,7 +4,7 @@ const pug = require('pug');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
-const config = require('../config/defaultConfig');
+// const config = require('../config/defaultConfig');
 const mime = require('./mime');
 const compress = require('./compress');
 const range = require('./range');
@@ -13,7 +13,7 @@ const isFresh = require('./cache');
 const tplPath = path.join(__dirname, '../template/dir.pug');
 const source = fs.readFileSync(tplPath,'utf-8');
 
-module.exports = async function (req, res, filePath) {
+module.exports = async function (req, res, filePath, config) {
     try {
         const stats = await stat(filePath);
         if (stats.isFile()){
